8df8a814ec3d373556672cb5ca023f93ab8666c3
Zane Darya <zanye.darya@gmail.com>
2023-06-19 22:43:33 +0300

    dads fav

--- test.py
+++ test.py
@@ -1,2 +1,18 @@
 print('Zane xan is awesome')
 print('Dad loves you!')
+
+# darya_favorite_daughter = 'Zane xan'
+
+# answer = input("Who is your father's favorite daughter?")
+
+# if answer == 'Zane xan':
+#     print("You are correct")
+# else:
+#     print('Please make a correct answer next time')
+
+name = 'Zane'
+
+if name is not None:
+    print('name is beautiful')
+else:
+    print('give name a value')
